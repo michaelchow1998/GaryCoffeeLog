d33c8a07cb41c4a89734e0d6912e99ee99c56cae
michaelchow1998 <chymichael.work@gmail.com>
2022-07-21 19:57:37 +0800

    Added: some log

--- src/main/java/com/garycoffee/garycoffeelog/controller/ProductLogController.java
+++ src/main/java/com/garycoffee/garycoffeelog/controller/ProductLogController.java
@@ -4,6 +4,7 @@ import com.garycoffee.garycoffeelog.dto.RequestLogProduct;
 import com.garycoffee.garycoffeelog.model.ProductLog;
 import com.garycoffee.garycoffeelog.service.ProductLogService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -15,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/product-log")
 @AllArgsConstructor
+@Slf4j
 public class ProductLogController {
 
     @Resource
@@ -22,6 +24,7 @@ public class ProductLogController {
 
     @PostMapping
     public ResponseEntity<String> logProductDetail(@RequestBody RequestLogProduct req){
+        log.info("Get request for create UserLog");
         String message = productLogService.logProductDetail(req);
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/v1/product-log").toUriString());
         return ResponseEntity.created(uri).body(message);
--- src/main/java/com/garycoffee/garycoffeelog/controller/UserLogController.java
+++ src/main/java/com/garycoffee/garycoffeelog/controller/UserLogController.java
@@ -4,6 +4,7 @@ import com.garycoffee.garycoffeelog.dto.RequestLogUser;
 import com.garycoffee.garycoffeelog.model.UserLog;
 import com.garycoffee.garycoffeelog.service.UserLogService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -15,13 +16,15 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/user-log")
 @AllArgsConstructor
+@Slf4j
 public class UserLogController {
 
     @Resource
     private UserLogService userLogService;
 
     @PostMapping
-    public ResponseEntity<String> logProductDetail(@RequestBody RequestLogUser req){
+    public ResponseEntity<String> logUser(@RequestBody RequestLogUser req){
+        log.info("Get request for create UserLog");
         String message = userLogService.logUserBalanceChange(req);
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/v1/user-log").toUriString());
         return ResponseEntity.created(uri).body(message);
